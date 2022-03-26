2cbf7d6c18ac60e3ebdccd4a1932a9e78b3146a4
Hyc <yhou17@simons-rock.edu>
2022-03-25 20:04:43 -0400

    Add class definition for deterministic finite automata

---
+++ src/DeterministicFiniteAutomaton.hpp
@@ -0,0 +1,32 @@
+#ifndef DETERMINISTIC_FINITE_AUTOMATON_HPP
+#define DETERMINISTIC_FINITE_AUTOMATON_HPP
+
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+
+using std::unordered_set;
+using std::unordered_map;
+using std::basic_string;
+
+namespace Automaton {
+	class DeterministicFiniteAutomaton {
+		public:
+			class State;
+			class Symbol;
+
+		private:
+			const unordered_set<const State *> stateSet;
+			const unordered_set<const Symbol *> alphabet;
+			const unordered_map<const State *, unordered_map<const Symbol *, const State *>> transitionFunction;
+			const State *initialState;
+			const unordered_set<const State *> acceptedStateSet;
+
+		public:
+			class State {};
+			class Symbol {};
+			DeterministicFiniteAutomaton(const unordered_set<const State *> &, const unordered_set<const Symbol *> &, const unordered_map<const State *, unordered_map<const Symbol *, const State *>> &, const State *, const unordered_set<const State *> &);
+	};
+};
+
+#endif /* DETERMINISTIC_FINITE_AUTOMATON_HPP */
